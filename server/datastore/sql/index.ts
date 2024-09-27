beadb79b1e1c4ffeca18ec7662b35c8aff40178c
AhmedSalman <medosalman375@gmial.com>
2024-09-27 12:35:40 +0300

    Enable foreign key constraints and implement post and return posts

--- server/datastore/sql/index.ts
+++ server/datastore/sql/index.ts
@@ -1,18 +1,21 @@
 import sqlite3 from 'sqlite3';
-import { open as sqliteOpen } from 'sqlite';
+import { open as sqliteOpen, Database } from 'sqlite';
 import path from 'path';
 
 import { Datastore } from '..';
 import { User, Post, Comment, Like } from '../../types';
 
 export class SqliteDataStore implements Datastore {
+  private db!: Database<sqlite3.Database, sqlite3.Statement>;
   public async openDb() {
-    const db = await sqliteOpen({
+    this.db = await sqliteOpen({
       filename: path.join(__dirname, 'technews.sqlite'),
       driver: sqlite3.Database,
     });
 
-    await db.migrate({
+    this.db.run('PRAGMA foreign_keys = ON;');
+
+    await this.db.migrate({
       migrationsPath: path.join(__dirname, 'migrations'),
     });
 
@@ -32,11 +35,20 @@ export class SqliteDataStore implements Datastore {
     throw new Error('Method not implemented.');
   }
   getPosts(): Promise<Post[]> {
-    throw new Error('Method not implemented.');
+    return this.db.all<Post[]>('SELECT * FROM posts;');
   }
-  createPost(post: Post): Promise<void> {
-    throw new Error('Method not implemented.');
+
+  async createPost(post: Post): Promise<void> {
+    await this.db.run(
+      'INSERT INTO posts (id, title, url, userId, postedAt) VALUES (?, ?, ?, ?, ?)',
+      post.id,
+      post.title,
+      post.url,
+      post.userId,
+      post.postedAt,
+    );
   }
+
   getPost(id: string): Promise<Post | undefined> {
     throw new Error('Method not implemented.');
   }
